5b65362bb25e57f36a8f91d873bfe521e245b128
thirteensuits <110221153+thirteensuits@users.noreply.github.com>
2024-09-14 14:17:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -52,4 +52,4 @@ app.post('/api/compileContract', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
